9fe3137a7ee23dd7da817c290720b160e22296fa
hozukiMk3 <meta.maetel@gmail.com>
2019-06-20 23:16:05 +0900

    Release準備。

---
+++ Rengeso/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Rengeso
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var clipboardText = ClipBoard;
+            if (clipboardText == null) return;
+
+            Console.WriteLine($"Debug:{clipboardText}");
+            clipboardText = clipboardText.Replace("\\", "/");
+            Console.WriteLine($"Debug:{clipboardText}");
+
+            Clipboard.SetText(clipboardText, TextDataFormat.UnicodeText);
+
+            Console.ReadKey();
+        }
+
+        private static string ClipBoard => Clipboard.ContainsText() ? Clipboard.GetText() : null;
+    }
+}
